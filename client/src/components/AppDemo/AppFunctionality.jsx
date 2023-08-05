b4b483c8df2e46857eaa61833b0425c99de33651
japhethbear <japhethbear@gmail.com>
2023-08-04 20:00:27 -0500

    Added videos to example section on home page

--- client/src/components/AppDemo/AppFunctionality.jsx
+++ client/src/components/AppDemo/AppFunctionality.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import LoginVideo from '../../assets/videos/LoginVideo.mp4'
+import YourRecipes from '../../assets/videos/YourRecipes.mp4'
+import AddRecipe from '../../assets/videos/AddRecipe.mp4'
 
 const AppFunctionality = () => {
   return (
@@ -20,24 +23,24 @@ const AppFunctionality = () => {
         >
           <div className="carousel-item">
             <video controls>
-              <source src="/path/to/video1.mp4" type="video/mp4" />
+              <source src={LoginVideo} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
             <p className='mt-3'>Register a new account or log in</p>
           </div>
           <div className="carousel-item">
             <video controls>
-              <source src="/path/to/video2.mp4" type="video/mp4" />
+              <source src={YourRecipes} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-            <p className='mt-3' >Add a recipe or go to your recipe dashboard</p>
+            <p className='mt-3' >View your recipe dashboard</p>
           </div>
           <div className="carousel-item">
             <video controls>
-              <source src="/path/to/video3.mp4" type="video/mp4" />
+              <source src={AddRecipe} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-            <p className='mt-3'>Browse your saved recipes</p>
+            <p className='mt-3'>Add your recipes</p>
             <div className="control-arrow-wrapper">
             </div>
             <div className="control-arrow-wrapper">
