1776e7f0c707644dc58676b4eef31a895122254d
japhethbear <japhethbear@gmail.com>
2023-08-18 14:58:44 -0500

    Another component updated

--- client/src/components/RecipeForm.jsx
+++ client/src/components/RecipeForm.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Link, useParams } from 'react-router-dom'
 import DropzoneComponent from './DropzoneComponent';
+import '../components/HomePage/homepagestyles.css'
+import kitchenBackground from '../assets/images/kitchenbackground.jpg'
+import cookbook from '../assets/images/cookbook.png'
+import gordonPassion from '../assets/images/gordonpassion.gif'
 
 const RecipeForm = () => {
 
@@ -98,167 +102,188 @@ const RecipeForm = () => {
     };
 
   return (
-    <div>
-        <div className='d-flex justify-content-around mt-4'>
-            <h1>Add My Own Recipe</h1>
-            <h5><Link to={`/api/${id}`} >Search for Recipe!</Link></h5>
-            <h5><Link to={`/home/${id}`} >Home Page</Link></h5>
-            <button className='btn btn-danger' style={logoutButtonStyle} onClick={logout}>Logout</button>
-        </div>
-        <form action="" className="col-md-6 mx-auto" onSubmit={handleSubmit}>
-            {errors.map((err, index) => <p key={index}>{err}</p>)}
-            <div className='form-group text-center mx-2'>
-                <label htmlFor='recipeName'>Recipe Name: </label>
-                <input type="text" className='form-control text-center' id='recipeName' name="recipeName" onChange={onChangeHandler}/>
-            </div>
-            <div className='form-group text-center mx-2'>
-                <label htmlFor='recipeMeal'>Recipe Meal: </label>
-                <select className='form-control text-center' id='recipeMeal' name="recipeMeal" onChange={onChangeHandler}>
-                    <option value="">--Please choose an option--</option>
-                    <option value="Breakfast">Breakfast</option>
-                    <option value="Lunch">Lunch</option>
-                    <option value="Dinner">Dinner</option>
-                    <option value="Snack">Snack</option>
-                </select>
-            </div>
-            {recipe.ingredients.map((ingredient, index) => (
-                <div key={index} className="form-group">
-                    <label htmlFor={`ingredientName${index}`}>Ingredient {index + 1} Name:</label>
-                    <input
-                    type="text"
-                    className="form-control text-center"
-                    id={`ingredientName${index}`}
-                    name="ingredientName"
-                    value={ingredient.ingredientName}
-                    onChange={(e) => {
-                        const ingredientsCopy = [...recipe.ingredients];
-                        ingredientsCopy[index] = {
-                        ...ingredientsCopy[index],
-                        ingredientName: e.target.value
-                        };
-                        setRecipe({
-                        ...recipe,
-                        ingredients: ingredientsCopy
-                        });
-                    }}
-                    />
-                    <label htmlFor={`ingredientAmount${index}`}>Ingredient {index + 1} Amount:</label>
-                    <input
-                    type="text"
-                    className="form-control text-center"
-                    id={`ingredientAmount${index}`}
-                    name="ingredientAmount"
-                    value={ingredient.ingredientAmount}
-                    onChange={(e) => {
-                        const ingredientsCopy = [...recipe.ingredients];
-                        ingredientsCopy[index] = {
-                        ...ingredientsCopy[index],
-                        ingredientAmount: e.target.value
-                        };
-                        setRecipe({
-                        ...recipe,
-                        ingredients: ingredientsCopy
-                        });
-                    }}
-                    />
+    <div className="dashboard-background-container" style={{ backgroundImage: `url(${kitchenBackground})` }}>
+            <div className="auth-navbar">
+                <div className="container">
+                <div className="auth-navbar-brand">
+                    <img src={cookbook} alt="Cook Book Picture" style={{ width: '40px', height: 'auto' }} />
+                    <h3>Recipe Book</h3>
                 </div>
-                ))}
-
-            <div className="form-group">
-                <label htmlFor="ingredientName">Ingredient Name:</label>
-                <input
-                    type="text"
-                    className="form-control text-center"
-                    id="ingredientName"
-                    name="ingredientName"
-                    value={currentIngredient.ingredientName}
-                    onChange={(e) => {
-                    setCurrentIngredient({
-                        ...currentIngredient,
-                        ingredientName: e.target.value
-                    });
-                    }}
-                />
-                <label htmlFor="ingredientAmount">Ingredient Amount:</label>
-                <input
-                    type="text"
-                    className="form-control text-center"
-                    id="ingredientAmount"
-                    name="ingredientAmount"
-                    value={currentIngredient.ingredientAmount}
-                    onChange={(e) => {
-                    setCurrentIngredient({
-                        ...currentIngredient,
-                        ingredientAmount: e.target.value
-                    });
-                    }}
-                />
-                <button
-                    className="btn btn-primary mt-2"
-                    onClick={(e) => {
-                    e.preventDefault();
-                    setRecipe({
-                        ...recipe,
-                        ingredients: [...recipe.ingredients, currentIngredient]
-                    });
-                    setCurrentIngredient({
-                        ingredientName: "",
-                        ingredientAmount: ""
-                    });
-                    }}>Add Ingredient
-                </button>
+                <div className="auth-navbar-links">
+                    <Link to={`/api/${id}`} className="auth-navbar-link">Search for Recipe</Link>
+                    <Link to={`/home/${id}`} className="auth-navbar-link" >Home Page</Link>
+                    <Link to="/" className="auth-navbar-link" onClick={logout}>Logout</Link>
+                </div>
+                </div>
+            </div>
+            <div className='d-flex justify-content-center mt-2'>
+                <h1 className="landing-heading">Add My Own Recipe</h1>
             </div>
 
-            {recipe.instructions.map((instruction, index) => (
-                <div key={index} className="form-group">
-                    <label htmlFor={`instruction${index}`}>Instruction:</label>
-                    <input
-                    type="text"
-                    className="form-control text-center"
-                    id={`instruction${index}`}
-                    name="instruction"
-                    value={instruction}
-                    onChange={(e) => {
-                        const instructionsCopy = [...recipe.instructions];
-                        instructionsCopy[index] = e.target.value;
-                        setRecipe({
-                        ...recipe,
-                        instructions: instructionsCopy
-                        });
-                    }}
-                    />
+        <div className='d-flex'>
+            <form action="" className="col-md-8 mx-auto px-4" onSubmit={handleSubmit}>
+                {errors.map((err, index) => <p key={index}>{err}</p>)}
+                <div className='form-group text-center mx-2'>
+                    <label htmlFor='recipeName'>Recipe Name: </label>
+                    <input type="text" className='form-control text-center' id='recipeName' name="recipeName" onChange={onChangeHandler}/>
                 </div>
-                ))}
+                <div className='form-group text-center mx-2'>
+                    <label htmlFor='recipeMeal'>Recipe Meal: </label>
+                    <select className='form-control text-center' id='recipeMeal' name="recipeMeal" onChange={onChangeHandler}>
+                        <option value="">--Please choose an option--</option>
+                        <option value="Breakfast">Breakfast</option>
+                        <option value="Lunch">Lunch</option>
+                        <option value="Dinner">Dinner</option>
+                        <option value="Snack">Snack</option>
+                    </select>
+                </div>
+                {recipe.ingredients.map((ingredient, index) => (
+                    <div key={index} className="form-group">
+                        <label htmlFor={`ingredientName${index}`}>Ingredient {index + 1} Name:</label>
+                        <input
+                        type="text"
+                        className="form-control text-center"
+                        id={`ingredientName${index}`}
+                        name="ingredientName"
+                        value={ingredient.ingredientName}
+                        onChange={(e) => {
+                            const ingredientsCopy = [...recipe.ingredients];
+                            ingredientsCopy[index] = {
+                            ...ingredientsCopy[index],
+                            ingredientName: e.target.value
+                            };
+                            setRecipe({
+                            ...recipe,
+                            ingredients: ingredientsCopy
+                            });
+                        }}
+                        />
+                        <label htmlFor={`ingredientAmount${index}`}>Ingredient {index + 1} Amount:</label>
+                        <input
+                        type="text"
+                        className="form-control text-center"
+                        id={`ingredientAmount${index}`}
+                        name="ingredientAmount"
+                        value={ingredient.ingredientAmount}
+                        onChange={(e) => {
+                            const ingredientsCopy = [...recipe.ingredients];
+                            ingredientsCopy[index] = {
+                            ...ingredientsCopy[index],
+                            ingredientAmount: e.target.value
+                            };
+                            setRecipe({
+                            ...recipe,
+                            ingredients: ingredientsCopy
+                            });
+                        }}
+                        />
+                    </div>
+                    ))}
 
                 <div className="form-group">
-                <label htmlFor="instruction">Instruction:</label>
-                <input
-                    type="text"
-                    className="form-control text-center"
-                    id="instruction"
-                    name="instruction"
-                    value={currentInstruction}
-                    onChange={(e) => setCurrentInstruction(e.target.value)}
-                />
+                    <label htmlFor="ingredientName">Ingredient Name:</label>
+                    <input
+                        type="text"
+                        className="form-control text-center"
+                        id="ingredientName"
+                        name="ingredientName"
+                        value={currentIngredient.ingredientName}
+                        onChange={(e) => {
+                        setCurrentIngredient({
+                            ...currentIngredient,
+                            ingredientName: e.target.value
+                        });
+                        }}
+                    />
+                    <label htmlFor="ingredientAmount">Ingredient Amount:</label>
+                    <input
+                        type="text"
+                        className="form-control text-center"
+                        id="ingredientAmount"
+                        name="ingredientAmount"
+                        value={currentIngredient.ingredientAmount}
+                        onChange={(e) => {
+                        setCurrentIngredient({
+                            ...currentIngredient,
+                            ingredientAmount: e.target.value
+                        });
+                        }}
+                    />
+                    <button
+                        className="btn btn-primary mt-2"
+                        onClick={(e) => {
+                        e.preventDefault();
+                        setRecipe({
+                            ...recipe,
+                            ingredients: [...recipe.ingredients, currentIngredient]
+                        });
+                        setCurrentIngredient({
+                            ingredientName: "",
+                            ingredientAmount: ""
+                        });
+                        }}>Add Ingredient
+                    </button>
                 </div>
-            <div>
-            <button
-                className="btn btn-primary mt-2"
-                onClick={addInstruction}
-                type='button'>
-                Add Instruction
-            </button>
+
+                {recipe.instructions.map((instruction, index) => (
+                    <div key={index} className="form-group">
+                        <label htmlFor={`instruction${index}`}>Instruction:</label>
+                        <input
+                        type="text"
+                        className="form-control text-center"
+                        id={`instruction${index}`}
+                        name="instruction"
+                        value={instruction}
+                        onChange={(e) => {
+                            const instructionsCopy = [...recipe.instructions];
+                            instructionsCopy[index] = e.target.value;
+                            setRecipe({
+                            ...recipe,
+                            instructions: instructionsCopy
+                            });
+                        }}
+                        />
+                    </div>
+                    ))}
+
+                    <div className="form-group">
+                    <label htmlFor="instruction">Instruction:</label>
+                    <input
+                        type="text"
+                        className="form-control text-center"
+                        id="instruction"
+                        name="instruction"
+                        value={currentInstruction}
+                        onChange={(e) => setCurrentInstruction(e.target.value)}
+                    />
+                    </div>
+                <div>
+                <button
+                    className="btn btn-primary mt-2"
+                    onClick={addInstruction}
+                    type='button'>
+                    Add Instruction
+                </button>
+                </div>
+                <div className="form-group text-center mt-2">
+                    Photos: 
+                    <DropzoneComponent handleFileDrop={handleFileDrop} />
+                </div>
+                
+                <button className='btn btn-info mt-2'>Add Recipe!</button>
+            </form>
+            <div className="col-md-4 d-flex mx-auto align-items-center">
+                <img
+                    src={gordonPassion}
+                    alt="Gordon Ramsey Passion GIF"
+                    style={{ width: '100%', maxWidth: '400px', padding: '20px' }}
+                />
             </div>
-            <div className="form-group text-center mt-2">
-                Photos: 
-                <DropzoneComponent handleFileDrop={handleFileDrop} />
-            </div>
+        </div>
+
 
 
             
-            
-            <button className='btn btn-info mt-2'>Add Recipe!</button>
-        </form>
     </div>
   )};
 
